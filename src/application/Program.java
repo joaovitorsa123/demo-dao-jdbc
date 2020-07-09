5f088761c5f8f7f04e7823bddd87ceab02ca6337
Joao Vitor da Silva Araujo <vitorsilvaaraujo96@gmail.com>
2020-07-09 10:26:25 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Department obj = new Department(1, "Book");
-		System.out.println(obj);
-		Seller seller = new Seller(21,"Bob","bob@gmail.com.br", new Date(), 3000.0, obj);
+		
 		
 		SellerDao sellerDao = new DaoFactory().createSellerDao(); 
+		
+		Seller seller = sellerDao.findById(3);
 		//o programa não conhece a implementação, apenas a interface
 		System.out.println(seller);
 
