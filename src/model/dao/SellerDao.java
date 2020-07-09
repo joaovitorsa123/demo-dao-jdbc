eb4eaae1cc3ca824f99b27e83ee71eae05493687
Joao Vitor da Silva Araujo <vitorsilvaaraujo96@gmail.com>
2020-07-08 22:25:53 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller seller);
+	void update (Seller seller);
+	void deletebyId(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll(Integer id);
+
+}
