39bdaea3ccf61ccff3f3baa8ec1444cb1dd8990b
Joao Vitor da Silva Araujo <vitorsilvaaraujo96@gmail.com>
2020-07-08 22:43:42 -0300

    SellerDaoJDBC and DaoFactor

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
 		Department obj = new Department(1, "Book");
 		System.out.println(obj);
 		Seller seller = new Seller(21,"Bob","bob@gmail.com.br", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = new DaoFactory().createSellerDao(); 
+		//o programa não conhece a implementação, apenas a interface
 		System.out.println(seller);
 
 	}
